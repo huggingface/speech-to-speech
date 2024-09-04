a680335745ee1811075f5ac6e1354591af5191de
rs545837 <114828377+rs545837@users.noreply.github.com>
2024-09-05 00:54:51 +0530

    update a comment

--- arguments_classes/facebookmms_tts_arguments.py
+++ arguments_classes/facebookmms_tts_arguments.py
@@ -5,7 +5,7 @@ class FacebookMMSTTSHandlerArguments:
     model_name: str = field(
         default="facebook/mms-tts-eng",
         metadata={
-            "help": "The model name to use. Default is 'facebook/mms-tts-hin'."
+            "help": "The model name to use. Default is 'facebook/mms-tts-eng'."
         },
     )
     tts_language: str = field(
