396c7e0e124b135d3bccb0e010c6e6d217ede3ba
Andres Marafioti <andimarafioti@gmail.com>
2024-09-25 00:08:37 +0200

    improve client

--- audio_streaming_client.py
+++ audio_streaming_client.py
@@ -13,7 +13,7 @@ import ssl
 @dataclass
 class AudioStreamingClientArguments:
     sample_rate: int = field(default=16000, metadata={"help": "Audio sample rate in Hz. Default is 16000."})
-    chunk_size: int = field(default=2048, metadata={"help": "The size of audio chunks in samples. Default is 1024."})
+    chunk_size: int = field(default=512, metadata={"help": "The size of audio chunks in samples. Default is 512."})
     api_url: str = field(default="https://yxfmjcvuzgi123sw.us-east-1.aws.endpoints.huggingface.cloud", metadata={"help": "The URL of the API endpoint."})
     auth_token: str = field(default="your_auth_token", metadata={"help": "Authentication token for the API."})
 
@@ -30,6 +30,7 @@ class AudioStreamingClient:
             "Content-Type": "application/json"
         }
         self.session_state = "idle"  # Possible states: idle, sending, processing, waiting
+        self.ws_ready = threading.Event()
 
     def start(self):
         print("Starting audio streaming...")
@@ -47,11 +48,14 @@ class AudioStreamingClient:
 
         ws_thread = threading.Thread(target=self.ws.run_forever, kwargs={'sslopt': {"cert_reqs": ssl.CERT_NONE}})
         ws_thread.start()
+        
+        # Wait for the WebSocket to be ready
+        self.ws_ready.wait()
 
         send_thread = threading.Thread(target=self.send_audio)
         play_thread = threading.Thread(target=self.play_audio)
 
-        with sd.InputStream(samplerate=self.args.sample_rate, channels=1, dtype='int16', callback=self.audio_callback, blocksize=self.args.chunk_size):
+        with sd.InputStream(samplerate=self.args.sample_rate, channels=1, dtype='int16', callback=self.audio_input_callback, blocksize=self.args.chunk_size):
             send_thread.start()
             play_thread.start()
 
@@ -69,13 +73,19 @@ class AudioStreamingClient:
 
     def on_open(self, ws):
         print("WebSocket connection opened.")
+        self.ws_ready.set()  # Signal that the WebSocket is ready
 
     def on_message(self, ws, message):
         # message is bytes
-        audio_np = np.frombuffer(message, dtype=np.int16)
-        for i in range(0, len(audio_np), self.args.chunk_size):
-            chunk = audio_np[i:i+self.args.chunk_size]
-            self.recv_queue.put(chunk)
+        if message == b'DONE':
+            print("listen")
+            self.session_state = "listen"
+        else:
+            print("processing")
+            print("recv queue size: ", self.recv_queue.qsize())
+            self.session_state = "processing"
+            audio_np = np.frombuffer(message, dtype=np.int16)
+            self.recv_queue.put(audio_np)
 
     def on_error(self, ws, error):
         print(f"WebSocket error: {error}")
@@ -83,89 +93,38 @@ class AudioStreamingClient:
     def on_close(self, ws, close_status_code, close_msg):
         print("WebSocket connection closed.")
 
-    def audio_callback(self, indata, frames, time, status):
-        self.send_queue.put(indata.copy())
-
     def send_audio(self):
         while not self.stop_event.is_set():
             if not self.send_queue.empty():
                 chunk = self.send_queue.get()
-                self.ws.send(chunk.tobytes(), opcode=websocket.ABNF.OPCODE_BINARY)
+                if self.session_state != "processing":
+                    self.ws.send(chunk.tobytes(), opcode=websocket.ABNF.OPCODE_BINARY)
+                else:
+                    self.ws.send([], opcode=websocket.ABNF.OPCODE_BINARY)  # handshake 
+            time.sleep(0.01)
+
+    def audio_input_callback(self, indata, frames, time, status):
+        self.send_queue.put(indata.copy())
+
+    def audio_out_callback(self, outdata, frames, time, status):
+        if not self.recv_queue.empty():
+            chunk = self.recv_queue.get()
+            
+            # Ensure chunk is int16 and clip to valid range
+            chunk_int16 = np.clip(chunk, -32768, 32767).astype(np.int16)
+            
+            if len(chunk_int16) < len(outdata):
+                outdata[:len(chunk_int16), 0] = chunk_int16
+                outdata[len(chunk_int16):] = 0
             else:
-                time.sleep(0.01)
-
-    def send_request(self, audio_data=None):
-        payload = {"input_type": "speech",
-                   "inputs": ""}
-
-        if audio_data is not None:
-            print("Sending audio data")
-            payload["inputs"] = base64.b64encode(audio_data).decode('utf-8')
-
-        if self.session_id:
-            payload["session_id"] = self.session_id
-            payload["request_type"] = "continue"
+                outdata[:, 0] = chunk_int16[:len(outdata)]
         else:
-            payload["request_type"] = "start"
-
-        try:
-            start_time = time.time()  # Record the start time
-            response = requests.post(self.args.api_url, headers=self.headers, json=payload)
-            end_time = time.time()  # Record the end time
-            response_time = end_time - start_time  # Calculate the response time
-            print(f"Response time: {response_time:.2f} seconds")  # Print the response time
-
-            response_data = response.json()
-
-            if "session_id" in response_data:
-                self.session_id = response_data["session_id"]
-
-            if "status" in response_data and response_data["status"] == "processing":
-                print("Processing audio data")
-                self.session_state = "processing"
-
-            if "output" in response_data and response_data["output"]:
-                print("Received audio data")
-                self.session_state = "processing"  # Set state to processing when we start receiving audio
-                audio_bytes = base64.b64decode(response_data["output"])
-                audio_np = np.frombuffer(audio_bytes, dtype=np.int16)
-                # Split the audio into smaller chunks for playback
-                for i in range(0, len(audio_np), self.args.chunk_size):
-                    chunk = audio_np[i:i+self.args.chunk_size]
-                    self.recv_queue.put(chunk)
-
-            if "status" in response_data and response_data["status"] == "completed":
-                print("Completed audio processing")
-                self.session_state = None
-                self.session_id = None
-                while not self.recv_queue.empty():
-                    time.sleep(0.01)  # wait for the queue to empty
-                with self.send_queue.mutex:
-                    self.send_queue.queue.clear()  # Clear the queue
-
-        except Exception as e:
-            print(f"Error sending request: {e}")
-            self.session_state = "idle"  # Reset state to idle in case of error
+            outdata[:] = 0
 
     def play_audio(self):
-        def audio_callback(outdata, frames, time, status):
-            if not self.recv_queue.empty():
-                chunk = self.recv_queue.get()
-                
-                # Ensure chunk is int16 and clip to valid range
-                chunk_int16 = np.clip(chunk, -32768, 32767).astype(np.int16)
-                
-                if len(chunk_int16) < len(outdata):
-                    outdata[:len(chunk_int16), 0] = chunk_int16
-                    outdata[len(chunk_int16):] = 0
-                else:
-                    outdata[:, 0] = chunk_int16[:len(outdata)]
-            else:
-                outdata[:] = 0
-
-        with sd.OutputStream(samplerate=self.args.sample_rate, channels=1, dtype='int16', callback=audio_callback, blocksize=self.args.chunk_size):
+        with sd.OutputStream(samplerate=self.args.sample_rate, channels=1, dtype='int16', callback=self.audio_out_callback, blocksize=self.args.chunk_size):
             while not self.stop_event.is_set():
-                time.sleep(0.01)
+                time.sleep(0.1)
 
 if __name__ == "__main__":
     import argparse
